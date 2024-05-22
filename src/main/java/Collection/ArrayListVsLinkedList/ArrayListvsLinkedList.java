7c0c87e84259f591b0a277d256bb19b72b78761d
DeeprajGawand <deeprajgawand@gmail.com>
2024-05-22 14:13:14 +0530

    arraylist done

---
+++ src/main/java/Collection/ArrayListVsLinkedList/ArrayListvsLinkedList.java
@@ -0,0 +1,12 @@
+package Collection.ArrayListVsLinkedList;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ArrayListvsLinkedList {
+    public static void main(String[] args) {
+
+        ArrayList arr = new ArrayList();
+
+    }
+}
